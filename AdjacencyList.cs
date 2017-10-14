2c9b785af759ce4dfffbb893dc00a53e4e4037d9
Huynh Do Huy Tap <huynh.dohuta@gmail.com>
2017-10-14 07:50:10 +0700

    Add files via upload

---
+++ AdjacencyList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAdjacencyList
+{
+    [Serializable]
+    public class AdjacencyList
+    {
+        private int vertices;
+        private LinkedList<int>[] list;
+
+        public int Vertices
+        {
+            get { return vertices; }
+            set { vertices = value; }
+        }
+        public LinkedList<int>[] List
+        {
+            get { return list; }
+            set { list = value; }
+        }
+
+        public AdjacencyList()
+        {
+            Vertices = 0;
+            List = null;
+        }
+
+        public AdjacencyList(AdjacencyList list)
+        {
+            this.Vertices = list.Vertices;
+            this.List = list.List;
+        }
+        
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append(Vertices); sb.AppendLine();
+            for (int i = 0; i < Vertices; i++)
+            {
+                foreach (var item in List[i])
+                {
+                    sb.Append($"{item + 1} ");
+                }
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+    }
+}
